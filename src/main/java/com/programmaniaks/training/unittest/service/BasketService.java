da006e1600cdf2f22c03b880cbbd09edf40af08e
Programmaniaks <programmaniaks@gmail.com>
2015-04-28 10:45:00 +0100

    Correction on BasketService definition. Correction of BasketControllerTest, configure well mockMvc run

--- src/main/java/com/programmaniaks/training/unittest/service/BasketService.java
+++ src/main/java/com/programmaniaks/training/unittest/service/BasketService.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.Map.Entry;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.programmaniaks.training.unittest.dao.ArticleDao;
@@ -13,6 +14,7 @@ import com.programmaniaks.training.unittest.exceptions.NotEnoughtQtyException;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+@Service
 public class BasketService {
 	
 	@Autowired
--- src/test/java/com/programmaniaks/training/unittest/controller/BasketControllerTest.java
+++ src/test/java/com/programmaniaks/training/unittest/controller/BasketControllerTest.java
@@ -4,19 +4,24 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.HashMap;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.programmaniaks.training.unittest.Application;
 import com.programmaniaks.training.unittest.entity.Article;
@@ -35,46 +40,26 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("file:src/test/resources/spring-test-context.xml")
+@ContextConfiguration("/spring-test-context.xml")
 @WebAppConfiguration
-public class BasketControllerTest {
+public class BasketControllerTest  {
+
 	@Autowired
-	private BasketController basketController;
+	private HttpMessageConverter<Object> mappingJackson2HttpMessageConverter;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
 	
-
-
-	public HttpMessageConverter getMappingJackson2HttpMessageConverter() {
-		return mappingJackson2HttpMessageConverter;
-	}
-
-	public void setMappingJackson2HttpMessageConverter(
-			HttpMessageConverter mappingJackson2HttpMessageConverter) {
-		this.mappingJackson2HttpMessageConverter = mappingJackson2HttpMessageConverter;
-	}
-
-	public MediaType getContentType() {
-		return contentType;
-	}
-
-	public void setContentType(MediaType contentType) {
-		this.contentType = contentType;
-	}
-
-	public MockMvc getMockMvc() {
-		return mockMvc;
-	}
-
-	public void setMockMvc(MockMvc mockMvc) {
-		this.mockMvc = mockMvc;
-	}
-
-	private HttpMessageConverter mappingJackson2HttpMessageConverter;
+	private MockMvc mockMvc;
 	
 	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
 												MediaType.APPLICATION_JSON.getSubtype(),
 												Charset.forName("utf8"));
 	
-	private MockMvc mockMvc;
+	@Before
+	public void setUp() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
 	
 	@Test
 	public void computeSumTest() throws Exception{
@@ -94,4 +79,29 @@ public class BasketControllerTest {
                 o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
         return mockHttpOutputMessage.getBodyAsString();
     }
+	
+	public HttpMessageConverter<Object> getMappingJackson2HttpMessageConverter() {
+		return mappingJackson2HttpMessageConverter;
+	}
+
+	public void setMappingJackson2HttpMessageConverter(
+			HttpMessageConverter<Object> mappingJackson2HttpMessageConverter) {
+		this.mappingJackson2HttpMessageConverter = mappingJackson2HttpMessageConverter;
+	}
+
+	public MediaType getContentType() {
+		return contentType;
+	}
+
+	public void setContentType(MediaType contentType) {
+		this.contentType = contentType;
+	}
+
+	public MockMvc getMockMvc() {
+		return mockMvc;
+	}
+
+	public void setMockMvc(MockMvc mockMvc) {
+		this.mockMvc = mockMvc;
+	}
 }
